b7d5629d3cb3eaaf11eb0ca91d988189a0195cf6
LiiNen <38001238+LiiNen@users.noreply.github.com>
2018-09-10 13:01:59 +0900

    2018.9.10 12:56
    show lyrics over the other things -> so that we can see lyrics over album art
    once click btn - load lyrics and show
    once again - save lyrics in string(->next time, do not load lyrics from server) and show null string

--- musicPlayerPrac/musicPlayerPrac/lyricsView.swift
+++ musicPlayerPrac/musicPlayerPrac/lyricsView.swift
@@ -11,28 +11,42 @@ import UIKit
 
 class lyricsView: UIViewController {
 
-    
+    var lyricsBool = Bool()
+    var firstLoadString = String()
     @IBOutlet weak var lyricsText: UITextView!
     
     @IBAction func getLyricsBtn(_ sender: Any) {
-        let url = "https://indi-list.com/api/GetLyrics/"
-        let para : Parameters = [ "mid" : "41768"]
-        let headers = ["x-access-token" : "eyJhbGciOiJSUzUxMiIsInR5cCI6IkpXVCJ9.eyJpZCI6InF3ZXIxMjM0IiwiZXhwIjoxNTY4MDM1ODY4LCJleHBfcmVmcmVzaCI6MTUzNjUwMzQ2OCwiaWF0IjoxNTM2NDk5ODY3fQ.BPtr3OS--50lwZaYPiC0VOaBMpqKLEsUQWXusOEtJJEOBVO8Kq-DGL993WyHdPz0tVK75EBPxwBjwOLAXfvosZNCOkssYBBLDnhDKEIZ_RtKh4gJZmUeOqG0tpNQUt_xC16fS7VnOn0uw9saERw8lKDVhEFiWR31ZrIw0UpKjbs"]
-        
-        Alamofire.request(url, method: .post, parameters: para, encoding: JSONEncoding.default, headers : headers).responseString { response in
-            //json return check in console && json to string
-            
-            let retString = (response.value ?? "")
-            var tempString = retString.components(separatedBy: "\"")
-            let tempStringLine = tempString[3].components(separatedBy: "\\n")
-            
-            self.lyricsText.text = ""
-            for line in tempStringLine{
-                self.lyricsText.text = (self.lyricsText.text ?? "") + " \n \(line) "
+        if(!lyricsBool){
+            if(firstLoadString == ""){
+                let url = "https://indi-list.com/api/GetLyrics/"
+                let para : Parameters = [ "mid" : "41768"]
+                let headers = ["x-access-token" : "eyJhbGciOiJSUzUxMiIsInR5cCI6IkpXVCJ9.eyJpZCI6InF3ZXIxMjM0IiwiZXhwIjoxNTY4MDg2OTM0LCJleHBfcmVmcmVzaCI6MTUzNjU1NDUzNCwiaWF0IjoxNTM2NTUwOTM0fQ.IavV9USJb3W_ymlD73TDViNfqn2R5uN0IAaPHQslDMZ8NjS4ZPnbbIWzdsLNjePwk3HFiOuPlofb9tF0KF6tAd8u2bBZqKho-9DLMo782XbrZltWEQ2p7smpZZaK0hWbv7y8lJGrMu47WHup1sVII3jN8TyoEIq93adicL-adzg"]
+                
+                Alamofire.request(url, method: .post, parameters: para, encoding: JSONEncoding.default, headers : headers).responseString { response in
+                    //json return check in console && json to string
+                    
+                    let retString = (response.value ?? "")
+                    var tempString = retString.components(separatedBy: "\"")
+                    let tempStringLine = tempString[3].components(separatedBy: "\\n")
+                    
+                    self.lyricsText.text = ""
+                    for line in tempStringLine{
+                        self.lyricsText.text = (self.lyricsText.text ?? "") + "\n\(line) "
+                    }
+                }
+            }
+            else{
+                self.lyricsText.text = firstLoadString
             }
             
-            
+            lyricsBool = true
         }
+        else{
+            firstLoadString = lyricsText.text!
+            self.lyricsText.text = ""
+            lyricsBool = false
+        }
+        
     }
     
     
@@ -40,6 +54,8 @@ class lyricsView: UIViewController {
         super.viewDidLoad()
         self.lyricsText.text = ""
 
+        lyricsBool = false
+        firstLoadString = ""
         // Do any additional setup after loading the view.
     }
 
